93aacb8b4193aab6ee2c6cfdd87056a67be5fe1b
James Harrod <james@voxo.co>
2023-08-07 19:46:38 -0500

    code improvements and promise based

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
-function check69(num) {
-  if (num === 69) {
+const isNumber = require("is-number");
+const isOdd = require("is-odd");
+const isEven = require("is-even");
+const isString = require("is-string");
+
+module.exports = async (value) => {
+
+  //return nice if nice is passed since nice === 69
+  if(isString(value) && value.toLowerCase() === "nice"){
     return "Nice";
   }
-}
 
-module.exports = check69;
+  //parse value to number in case a string is passed
+  let number = +value;
+
+  //check and make sure we have a number
+  if (!isNumber(number)) {
+    return "Not Nice";
+  }
+
+  //conver the number to a string
+  number = number.toString();
+  const charCount = number.length;
+  const charArray = number.split('').map((char) => +char);
+
+  //69 only has two digits
+  if (charCount !== 2) {
+    return "Not Nice";
+  }
+
+  //check characters at each position
+  const validCharacterIndex = [6, 9];
+  for (let i = 0; i < charCount; i++) {
+    if (charArray[i] !== validCharacterIndex[i]) {
+      return "Not Nice";
+    }
+  }
+
+  //convert back to a number
+  number = +number;
+
+  //69 is not even
+  if (isEven(number)) {
+    return "Not Nice";
+  }
+
+  //69 is odd
+  if (!isOdd(number)) {
+    return "Not Nice";
+  }
+
+  if (number === 69) {
+    return "Nice";
+  }
+
+  return "Not Nice";
+
+}
